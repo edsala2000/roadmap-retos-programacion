3707bc2b678ed088a9ac90b3ed6bfad060935cfd
edsala2000 <edsala2000@gmail.com>
2025-02-05 16:56:31 -0500

    Create edsala2000.py
    #00-python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/edsala2000.py
@@ -0,0 +1,15 @@
+# https://python.org/downloads/
+# comentario en una linea
+"""
+esto tambien es 
+un comentario
+en varias lineas
+"""
+'''
+esto tambien es
+un comentario en 
+varias lineas
+'''
+my_varible = "esta es mi variable"
+my_variable = " esta es mi nueva variable"
+MY_CONSTANT = " MI CONSTANTE" # POR CONVENCION
